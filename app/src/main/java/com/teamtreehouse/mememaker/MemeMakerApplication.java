159e3ec39df882ef6ff06819e6569fba8840e0d7
Paul Anton <paul.antton@gmail.com>
2014-12-02 16:07:39 +0200

    Created and added setting page. Quick UI for the user to modify the shared pref

--- app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplication.java
+++ app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplication.java
@@ -1,5 +1,7 @@
 package com.teamtreehouse.mememaker;
 
+import android.preference.PreferenceManager;
+
 import com.teamtreehouse.mememaker.utils.FileUtilities;
 
 import java.io.File;
@@ -18,5 +20,9 @@ public class MemeMakerApplication extends android.app.Application {
         FileUtilities.saveAssetImage(this, "dogmess.jpg");
         FileUtilities.saveAssetImage(this, "excitedcat.jpg");
         FileUtilities.saveAssetImage(this, "guiltypup.jpg");
+
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences,false);
+        
     }
 }
